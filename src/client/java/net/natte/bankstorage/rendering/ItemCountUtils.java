d97ea49b0de1b653282c89dbf36767958f7f190e
Natanaelel <43886350+Natanaelel@users.noreply.github.com>
2024-05-17 04:16:17 +0200

    "snap" font scale to gui scale

--- src/client/java/net/natte/bankstorage/rendering/ItemCountUtils.java
+++ src/client/java/net/natte/bankstorage/rendering/ItemCountUtils.java
@@ -1,7 +1,6 @@
 package net.natte.bankstorage.rendering;
 
 import net.minecraft.client.MinecraftClient;
-import net.natte.bankstorage.util.Util;
 
 public class ItemCountUtils {
 
@@ -10,7 +9,7 @@ public class ItemCountUtils {
 	public static String toConsiseString(int count) {
 		int index = 0;
 		if (count > 9999) {
-			while (count / 1000 != 0) {
+			while (count >= 1000) {
 				count /= 1000;
 				index++;
 			}
@@ -25,25 +24,18 @@ public class ItemCountUtils {
 
 	/**
 	 * How big or small to render the item count text
-	 * 
-	 * @return 1 == vanilla
+	 * 1 == vanilla
 	 */
 	public static float scale(String string) {
 		MinecraftClient client = MinecraftClient.getInstance();
-		int guiScale = client.options.getGuiScale().getValue();
-		// return new float[][]{new float[]{0}}[guiScale][0];
-		
-		// System.out.println(string);
-		// float factorDependingOnTextLength = string.length() >= 4 ? 3f/5f : string.length() >= 3 ? 5f/6f : 1f;
-		int length = string.length();
-		float factorDependingOnTextLength = length >= 4 ? 2f/3f : length >= 3 ? 0.9f : 1f;
-		return Util.isDebugMode ? 5f / 8f : guiScale == 1 ? length >= 4 ? 0.7f : length >= 3 ? 1f : 1f : guiScale == 2 ? length >= 4 ? 4f/6f : length >= 3 ? 5f/6f : 1f : Math.max(1f, (float)(int)(guiScale * factorDependingOnTextLength)) / guiScale;
-		// if (string.length() > 3) {
-		// } else if (string.length() == 3) {
-		// 	// return 6f / 8f;
-		// 	// return (guiScale - 1f) / guiScale;
-		// 	return Util.isDebugMode ?  5f / 8f : (guiScale - 1f) / guiScale;
-		// }
-		// return 1f;
+		int guiScale = (int) client.getWindow().getScaleFactor();
+		float width = client.textRenderer.getTextHandler().getWidth(string);
+		float scale = 16f / width;
+		float step = Math.max(3, guiScale);
+		return Math.min(1f, scaledFloor(scale, step));
+	}
+
+	private static float scaledFloor(float value, float step) {
+		return ((float) (int) (value * step)) / step;
 	}
 }
