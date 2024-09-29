9d772066023473b0940eb87a62aeeccf9888489f
Natanaelel <43886350+Natanaelel@users.noreply.github.com>
2024-09-29 15:09:15 +0200

    log info when storage could not be found

--- src/main/java/net/natte/bankstorage/packet/server/RequestBankStoragePacketC2S.java
+++ src/main/java/net/natte/bankstorage/packet/server/RequestBankStoragePacketC2S.java
@@ -8,6 +8,7 @@ import net.minecraft.network.codec.ByteBufCodecs;
 import net.minecraft.network.codec.StreamCodec;
 import net.minecraft.network.protocol.common.custom.CustomPacketPayload;
 import net.minecraft.server.level.ServerPlayer;
+import net.natte.bankstorage.BankStorage;
 import net.natte.bankstorage.container.BankItemStorage;
 import net.natte.bankstorage.packet.NetworkUtil;
 import net.natte.bankstorage.util.Util;
@@ -31,6 +32,10 @@ public record RequestBankStoragePacketC2S(UUID uuid, short cachedRevision) imple
     public static void handle(RequestBankStoragePacketC2S packet, IPayloadContext context) {
         ServerPlayer player = (ServerPlayer) context.player();
         BankItemStorage bankItemStorage = Util.getBankItemStorage(packet.uuid);
+        if (bankItemStorage == null) {
+            BankStorage.LOGGER.info("{} at {} {} requested cache update for {} revision {} which doesn't exist. This is *impossible*. ignoring.", context.player().getName().getString(), context.player().blockPosition(), context.player().level(), packet.uuid, packet.cachedRevision);
+            return;
+        }
         // only send update if client doesn't already have latest version (revision)
         if (packet.cachedRevision != bankItemStorage.getRevision()) {
             NetworkUtil.syncCachedBankS2C(packet.uuid, player);
